a36d9e5ef1180c8e4d37dc3d1207763cef31b40e
Ruben-David Kraus <ruben-david.kraus@ottogroup.com>
2025-03-15 15:12:22 +0100

    teams view

--- pages/orga/teams/[name].vue
+++ pages/orga/teams/[name].vue
@@ -1,10 +1,49 @@
-<template></template>
+<template>
+  <div>
+    <header class="bg-primary text-primary-foreground shadow-md">
+      <div class="container mx-auto py-6 px-4">
+        <h1 class="text-3xl font-bold">Team Details</h1>
+      </div>
+    </header>
+    <main class="container mx-auto px-4 py-8 space-y-8">
+      <TeamInfoCard
+        v-if="team"
+        :team="team"
+        :games-played="team.totalMatches"
+        :wins="team.wins"
+      />
+      <p class="text-xl font-semibold text-foreground">Spieler</p>
+      <div class="grid lg:grid-cols-2 gap-3" v-if="team && team.players">
+        <PlayerInfoCard
+          v-for="player in team.players"
+          :player="player"
+          :games-played="player?.gamesPlayed ?? 0"
+          :plays-in="player.team ?? ''"
+          :wins="player?.wins ?? 0"
+        />
+      </div>
+    </main>
+  </div>
+</template>
 <script setup lang="ts">
+const route = useRoute();
+const nuxtApp = useNuxtApp();
+
 useHead({
-  title: `Team ${decodeURIComponent(useRoute().params.name as string)}`,
+  title: `Team ${decodeURIComponent(route.params.name as string)}`,
 });
 
 definePageMeta({
   middleware: "auth",
 });
+
+const { data: team } = await useLazyFetch(
+  () => `/api/teams/${encodeURIComponent(route.params.publicID as string)}`,
+  {
+    getCachedData(key) {
+      // TODO: Fix this to displayed the time when the data was cached
+      return getCachedDataOrFetch(key, nuxtApp);
+    },
+  }
+);
 </script>
