ee548068977f493a846e78bd15ec0698dd84efca
Ruben <kraus.rd@icloud.com>
2025-05-28 13:44:51 +0200

    fix: lockfile

--- components/match/GroupStageMatchesCard.vue
+++ components/match/GroupStageMatchesCard.vue
@@ -3,9 +3,8 @@
     <CardHeader>
       <CardTitle>Spiele {{ group }}</CardTitle>
     </CardHeader>
-    <CardContent
-      class="grid grid-cols-1 md:grid-cols-2 gap-4 px-2 md:px-6"
-    >
+    <CardContent class="grid grid-cols-1 md:grid-cols-2 gap-4 px-2 md:px-6">
+      <span class="col-span-full">Teams: {{ teams.join(", ") }}</span>
       <MatchCard
         v-for="match in matches"
         :match-id="match.matchId"
@@ -24,4 +23,13 @@ const props = defineProps<{
   group: string;
   matches: Match[];
 }>();
+
+const teams = computed(() => {
+  const teamSet = new Set<string>();
+  props.matches.forEach((match) => {
+    if (match.homeTeam?.name) teamSet.add(match.homeTeam.name);
+    if (match.awayTeam?.name) teamSet.add(match.awayTeam.name);
+  });
+  return Array.from(teamSet).sort();
+});
 </script>
