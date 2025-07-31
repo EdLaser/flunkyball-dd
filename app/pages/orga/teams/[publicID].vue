de47b09d5bae3bd06a84843fc5e261dbda51365d
Ruben Kraus <s80350@htw-dresden.de>
2025-07-31 11:49:00 +0200

    update

--- app/pages/orga/teams/[publicID].vue
+++ app/pages/orga/teams/[publicID].vue
@@ -27,7 +27,6 @@
 </template>
 <script setup lang="ts">
 const route = useRoute();
-const nuxtApp = useNuxtApp();
 
 useHead({
   title: `Team ${decodeURIComponent(route.params.publicID as string)}`,
@@ -38,12 +37,6 @@ definePageMeta({
 });
 
 const { data: team } = await useLazyFetch(
-  () => `/api/teams/${encodeURIComponent(route.params.publicID as string)}`,
-  {
-    getCachedData(key) {
-      // TODO: Fix this to displayed the time when the data was cached
-      return getCachedDataOrFetch(key, nuxtApp);
-    },
-  }
+  () => `/api/teams/${encodeURIComponent(route.params.publicID as string)}`
 );
 </script>
