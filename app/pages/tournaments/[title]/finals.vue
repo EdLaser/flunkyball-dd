845fb554dccc910b6857603da2bdfde96eaed521
Ruben Kraus <s80350@htw-dresden.de>
2025-08-02 15:21:25 +0200

    update

--- app/pages/tournaments/[title]/finals.vue
+++ app/pages/tournaments/[title]/finals.vue
@@ -16,43 +16,42 @@
             </TabsTrigger>
           </TabsList>
           <TabsContent value="matches">
-            <div
-              v-if="finalMatches && finalMatches?.length > 0"
-              class="space-y-4"
-            >
+            <ClientOnly>
               <div
-                v-for="finalMatch in finalMatches"
-                :key="finalMatch.stage_name"
+                v-if="finalMatches"
+                class="space-y-4"
               >
-                <h2 class="text-2xl font-bold mb-2">{{ finalMatch.stage_name }}</h2>
-                <MatchCard
-                  class="mb-4"
-                  v-for="match in finalMatch.matches"
-                  :match-id="match.match_id"
-                  :away-team-name="{
-                    id: match.teams_matches_away_team_idToteams.id,
-                    name: match.teams_matches_away_team_idToteams.name,
-                  }"
-                  :home-team-name="{
-                    id: match.teams_matches_home_team_idToteams.id,
-                    name: match.teams_matches_home_team_idToteams.name,
-                  }"
-                  :winner-team-name="
-                    match.teams_matches_match_winnerToteams
-                      ? {
-                          id: match.teams_matches_match_winnerToteams.id,
-                          name: match.teams_matches_match_winnerToteams.name,
-                        }
-                      : null
-                  "
-                />
+                <div
+                  v-for="finalMatch in finalMatches"
+                  :key="finalMatch.stage_name"
+                >
+                  <h2 class="text-2xl font-bold mb-2">
+                    {{ finalMatch.stage_name }}
+                  </h2>
+                  <MatchCard
+                    class="mb-4"
+                    v-for="match in finalMatch.matches"
+                    :match-id="match.match_id"
+                    :away-team-name="{
+                      id: match.teams_matches_away_team_idToteams.id,
+                      name: match.teams_matches_away_team_idToteams.name,
+                    }"
+                    :home-team-name="{
+                      id: match.teams_matches_home_team_idToteams.id,
+                      name: match.teams_matches_home_team_idToteams.name,
+                    }"
+                    :winner-team-name="
+                      match.teams_matches_match_winnerToteams
+                        ? {
+                            id: match.teams_matches_match_winnerToteams.id,
+                            name: match.teams_matches_match_winnerToteams.name,
+                          }
+                        : null
+                    "
+                  />
+                </div>
               </div>
-            </div>
-            <div v-else>
-              <p class="text-muted-foreground">
-                Keine Spiele in dieser Gruppenphase gefunden.
-              </p>
-            </div>
+            </ClientOnly>
           </TabsContent>
         </Tabs>
       </main>
@@ -61,14 +60,14 @@
 </template>
 
 <script lang="ts" setup>
-import { Swords } from 'lucide-vue-next';
+import { Swords } from "lucide-vue-next";
 const title = useRoute().params.title as string;
 
 useHead({
-  title: `Gruppenphase: ${decodeURIComponent(title)}`,
+  title: `Finale: ${decodeURIComponent(title)}`,
 });
 
 const { data: finalMatches } = await useFetch(
-  `/api/tournaments/${title}/finals/matches`
+  () => `/api/tournaments/${title}/finals/matches`
 );
 </script>
